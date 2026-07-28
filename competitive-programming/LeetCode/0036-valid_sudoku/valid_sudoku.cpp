f3a8a17b303e4218d24a6283d415ca0aa5dad38b
razi-gp <zirachwidh@gmail.com>
2026-07-28 16:30:26 +0800

    feat: LeetCode 36 - Valid Sudoku

---
+++ competitive-programming/LeetCode/0036-valid_sudoku/valid_sudoku.cpp
@@ -0,0 +1,42 @@
+using namespace std;
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+class Solution 
+{
+    public:
+        bool isValidSudoku(vector<vector<char>>& board)
+        {
+            vector<vector<bool>> rows(9, vector<bool>(9, false));
+            vector<vector<bool>> cols(9, vector<bool>(9, false));
+            vector<vector<bool>> square(9, vector<bool>(9, false));
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i][j] != '.')
+                    {
+                        int v = board[i][j] - '1';
+
+                        if (rows[i][v]) return false;
+                        else rows[i][v] = true;
+
+                        if (cols[i][v]) return false;
+                        else cols[i][v] = true;
+
+                        int idx = i / 3 * 3 + j / 3;
+                        
+                        if (square[idx][v]) return false;
+                        else square[idx][v] = true;
+                    }
+                }
+            }
+
+            return true;
+        }
+};
